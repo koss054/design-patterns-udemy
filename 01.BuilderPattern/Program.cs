5f08f064bf005a21af2a33f2a74fd6a2d0bcc1ad
koss054 <konstantin054@abv.bg>
2023-02-08 19:13:28 +0200

    Complete solution for the builder pattern challenge

--- 01.BuilderPattern/Program.cs
+++ 01.BuilderPattern/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BuilderPattern
+{
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var firstBuilder = new CodeBuilder("FirstClass")
+                .AddField("Name", "string")
+                .AddField("Age", "int");
+
+            Console.WriteLine(firstBuilder);
+        }
+    }
+
+    public class Field
+    {
+        public string Name;
+        public string Type;
+
+        public override string ToString()
+        {
+            return $"public {Type} {Name}";
+        }
+    }
+
+    public class Class
+    {
+        public string Name;
+        public List<Field> Fields = new List<Field>();
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"public class {Name}")
+              .AppendLine("{");
+
+            foreach (var field in Fields)
+            {
+                sb.AppendLine($"  {field};");
+            }
+
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+    }
+
+    public class CodeBuilder
+    {
+        private Class _class = new Class();
+
+        public CodeBuilder(string rootName)
+        {
+            _class.Name = rootName;
+        }
+
+        public CodeBuilder AddField(string name, string type)
+        {
+            _class.Fields.Add(new Field { Name = name, Type = type });
+            return this;    // Fluent builder, allowing us to chain this method with itself.
+        }
+
+        public override string ToString()
+        {
+            return _class.ToString();
+        }
+    }
+}
