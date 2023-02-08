8ed6879af9a56d63e0afcf54fc9e9f4377a3c942
koss054 <konstantin054@abv.bg>
2023-02-08 21:50:57 +0200

    Complete Factory Pattern challenge

---
+++ 02.FactoryPattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pf = new PersonFactory();
+            var people = new List<Person>();
+
+            Person firstPerson = pf.CreatePerson("Koss");
+            Person secondPerson = pf.CreatePerson("Miki");
+            Person thirdPerson = pf.CreatePerson("Damqnski");
+            Person fourthPerson = pf.CreatePerson("Vanki4a");
+            Person fifthPerson = pf.CreatePerson("Dakata");
+            Person sixthPerson = pf.CreatePerson("Bili");
+
+            people.Add(firstPerson);
+            people.Add(secondPerson);
+            people.Add(thirdPerson);
+            people.Add(fourthPerson);
+            people.Add(fifthPerson);
+            people.Add(sixthPerson);
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Id}-{person.Name}");
+            }
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonFactory
+    {
+        private int personId;
+
+        public PersonFactory()
+        {
+            personId = 0;
+        }
+
+        public Person CreatePerson(string name)
+        {
+            var person = new Person()
+            {
+                Id = personId,
+                Name = name,
+            };
+
+            personId++;
+            return person;
+        }
+    }
+}
