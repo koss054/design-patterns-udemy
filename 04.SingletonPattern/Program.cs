949a3ec04355a406d6a23e850cac2f2f68e143da
koss054 <konstantin054@abv.bg>
2023-02-09 10:38:06 +0200

    Complete singleton coding challenge

---
+++ 04.SingletonPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var testObject = new object();
+            bool isSingleton = SingletonTester.IsSingleton(() => testObject);
+
+            Console.WriteLine(isSingleton);
+        }
+    }
+
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var firstObject = func();
+            var secondObject = func();
+
+            return ReferenceEquals(firstObject, secondObject);
+        }
+    }
+}
